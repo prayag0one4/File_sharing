684c0b58cd57fc45ef7413bb3ca1fd4759f5d82c
prayag0one4 <142918263+prayag0one4@users.noreply.github.com>
2025-05-22 21:19:24 +0530

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,10 @@
-export function Footer() {
+export const Footer = ()=> {
   return (
+    <footer>   
     <div className="fixed bottom-[10px] w-full flex justify-center">
       <p className="text-white">Created by <a className="text-blue-800 font-semibold" href="https://www.linkedin.com/in/prayag-raj22/" target="_blank" >@Prayag Raj</a></p>
     </div>
+    </footer>
+
   );
 }
